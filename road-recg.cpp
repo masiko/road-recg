5e41159b8760594617dbb9ac1e47dbcec8422cd9
masiko <masiko.taiitirou@gmail.com>
2016-04-24 11:30:28 +0900

    add color histogram

--- road-recg.cpp
+++ road-recg.cpp
@@ -8,13 +8,36 @@ roadRecg::roadRecg(int row, int col) {
 	result = cv::Mat::zeros(row,col,CV_8UC3);
 }
 
-int roadRecg::mainloop(cv::Mat in, cv::Mat out) {
+int roadRecg::setTbl(int w, int h) {
+	int result_bin=0;
+	int result_count=0;
 
-//	int row, col;
+	for (int i=0; i<16; i++)	histogram[i] = 0;
+
+	if (w>=width || h>=height)	return -1;
+
+	for (int i=height-h; i<height; i++) {
+		for (int j=width/2-w/2; j<width/2+w/2; j++) {
+			histogram[int(gray.data[j+i*width]/16)] += 1;
+		}
+	}
+	for (int i=0; i<16; i++) {
+		if (histogram[i] >= result_count) {
+			result_bin = i;
+			result_count = histogram[i];
+		}
+	}
+	return result_bin;
+}
+
+
+int roadRecg::mainloop(cv::Mat in, cv::Mat out) {
+	int edge_size = 0;
 	cv::cvtColor(in,gray,CV_RGB2GRAY);
 	result = cv::Mat::zeros(height,width,CV_8UC3);
 
-	detectRateOfChange(100);
+	detectRoadEdge(10);
+	edge_size = detectRateOfChange(100);
 
 	for (int r=0; r<height; r++) {
 		for (int c=0; c<width; c++) {
@@ -22,11 +45,12 @@ int roadRecg::mainloop(cv::Mat in, cv::Mat out) {
 		}
 	}
 
+/*	if (edge_size>20)	cv::HoughLinesP( edge, lines, 3, CV_PI/45, 5, 10, 10);
 	for (int c=0; c<lines.size(); c++) {
 		cv::line(result, cv::Point(lines[c][0], lines[c][1]),
 					cv::Point(lines[c][2], lines[c][3]), cv::Scalar(0,255,0), 3, 8);
 	}
-
+*/
 	cv::imshow("result", result);
 
 	return 0;
@@ -37,7 +61,7 @@ int roadRecg::detectRateOfChange(int thre) {
 	const int MASK_HALF = 5;
 	int first=0, end=0;
 	int diff = 0;
-	int edge_count = 0;
+	int edge_size = 0;
 	char edge_flag = 1;
 /*
 	for (int e=0; e<MASK_HALF-1; e++) {
@@ -61,7 +85,7 @@ int roadRecg::detectRateOfChange(int thre) {
 				if (edge_flag) {
 					edge.data[d + c*width] = 255; 
 					result.data[3*d + 3*c*width+2] = 255;
-					edge_count++;
+					edge_size++;
 					edge_flag = 0;
 				} else {
 					edge.data[d + c*width] = 0; 
@@ -80,7 +104,30 @@ int roadRecg::detectRateOfChange(int thre) {
 		first = 0;
 		end = 0;
 	}
-	if (edge_count>20)	cv::HoughLinesP( edge, lines, 3, CV_PI/45, 5, 30, 10);
-	return 0;
+//	if (edge_size>20)	cv::HoughLinesP( edge, lines, 3, CV_PI/45, 5, 10, 10);
+	return edge_size;
 }
 
+int roadRecg::detectRoadEdge(int num) {
+	const int MASK = 5;
+	int interval = height/(2*num);
+	int x=width/2;
+	int y;
+	int col_hist = setTbl(20, 40);
+	
+	std::cout<<col_hist<<"\n";
+
+	for (int i=0; i<num; i++) {
+		y = width*(i*interval+height/2);
+		for (int j=MASK; j<width/2; j+=MASK) {
+			 std::cout<<(gray.data[x+j + y]/16)<<",";
+			 if ((gray.data[x+j + y]/16)==col_hist){
+				 result.data[3*(x+j-MASK + y)] = 255;
+				 result.data[3*(x+j-MASK + y)+1] = 255;
+				 result.data[3*(x+j-MASK + y)+2] = 255;
+			 }
+		}
+		std::cout<<"\n";
+	}
+	return 0;
+}
--- road-recg.hpp
+++ road-recg.hpp
@@ -9,13 +9,15 @@ class roadRecg {
 		cv::Mat gray;		//gray scale input image
 		cv::Mat edge;		//edge detection image
 		cv::Mat result;		//output image, CV_8U3C
-		char color_tbl[765];	//color = b + 255*g + 2*255*r
+		int histogram[16];
+//		char color_tbl[765];	//color = b + 255*g + 2*255*r
 		cv::vector<cv::Vec4i> lines;
 	public:
 
 	roadRecg(int r, int c);
-	int setTbl(char map[765]);
+	int setTbl(int w, int h);
 	int mainloop(cv::Mat in, cv::Mat out);
 	int detectRateOfChange(int thre);
+	int detectRoadEdge(int num);
 };
 
